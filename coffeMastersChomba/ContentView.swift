162471cf9927c288cab862ac6190e1c2a1b6391c
chomba <chomba@bongohive.co.zm>
2022-04-21 20:26:19 +0200

    Initial Commit

---
+++ coffeMastersChomba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffeMastersChomba
+//
+//  Created by Chomba Chanda on 4/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coffeMastersChomba/coffeMastersChombaApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeMastersChombaApp.swift
+//  coffeMastersChomba
+//
+//  Created by Chomba Chanda on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct coffeMastersChombaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
